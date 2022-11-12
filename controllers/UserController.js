e8aa73ef8aed7ed78b33de081394158103979ae7
gabrielodd <gabrieloddone@id.uff.br>
2022-11-11 23:56:28 -0300

    Validações de atualização de usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,7 @@ const User = require('../models/User')
 //helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -133,7 +134,53 @@ module.exports = class UserController {
   }
 
   static async editUser(req, res){
-    res.status(200).json({message: 'Deu certo update!'})
-    return
+    const id = req.params.id
+
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const {name, email, phone, password, confirmpassword} = req.body
+
+    let image = ""
+
+    if(!name) {
+      res.status(422).json({message: 'O nome é obrigatório'})
+      return
+    }
+
+    if(!email) {
+      res.status(422).json({message: 'O email é obrigatório'})
+      return
+    }
+
+    const userExists = await User.findOne({email: email})
+
+    if(!user.email !== email && userExists) {
+      res.status(422).json({message: 'E-mail incorreto!'})
+      return
+    }
+
+    user.email = email
+
+    if(!phone) {
+      res.status(422).json({message: 'O telefone é obrigatório'})
+      return
+    }
+
+    if(!password) {
+      res.status(422).json({message: 'A senha é obrigatório'})
+      return
+    }
+
+    if(!confirmpassword) {
+      res.status(422).json({message: 'A confirmação de senha é obrigatório'})
+      return
+    }
+
+    if(password !== confirmpassword) {
+      res.status(422).json({message: 'A senha e confirmação de senha devem ser iguais.'})
+      return
+    }
+
   }
 }
