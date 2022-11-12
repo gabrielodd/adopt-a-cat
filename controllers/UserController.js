a80c0aa690fb8c7dfb3431a41546dfc1cf805562
gabrielodd <gabrieloddone@id.uff.br>
2022-11-12 16:41:06 -0300

    Finalizando atualização

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -167,6 +167,8 @@ module.exports = class UserController {
       return
     }
 
+    user.phone = phone
+
     if(!password) {
       res.status(422).json({message: 'A senha é obrigatório'})
       return
@@ -180,6 +182,26 @@ module.exports = class UserController {
     if(password !== confirmpassword) {
       res.status(422).json({message: 'A senha e confirmação de senha devem ser iguais.'})
       return
+    } else if(password === confirmpassword && password != null) {
+      const salt = await bcrypt.genSalt(12)
+      const passwordHash = await bcrypt.hash(password, salt)
+
+      user.password = passwordHash
+    }
+
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id},
+        { $set: user },
+        { new: true},
+      )
+    res.status(200).json({
+      message: "Usuário atualizado com sucesso!"
+    })
+    } catch (err) {
+      res.status(500).json({message: err})
+      return
+      
     }
 
   }
