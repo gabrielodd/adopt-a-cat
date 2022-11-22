127be391315d3f174ec9a4b6d5afcffdb1ae19ae
gabrielodd <gabrieloddone@id.uff.br>
2022-11-22 01:34:45 -0300

    Pegar todos os pets adotados pelo usuário

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -69,4 +69,13 @@ module.exports = class PetController {
 
     res.status(200).json({pets: pets})
   }
+
+  static async getAllUserAdoptions(req, res) {
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAt')
+
+    res.status(200).json({pets})
+  }
 }
--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -12,5 +12,6 @@ router.post('/create',
 )
 router.get('/', PetController.getAll)
 router.get('/mypets', verifyToken, PetController.getAllUserPets)
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
 
 module.exports = router
