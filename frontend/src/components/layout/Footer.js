9d4e6be34bd3fb395d22baf4b351ed43dc6b4234
gabrielodd <gabrieloddone@id.uff.br>
2022-12-04 23:17:57 -0300

    CSS Navbar e Footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css'
+
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <span className="bold">Adopt A Cat &copy; 2022</span>
     </footer>
   )
