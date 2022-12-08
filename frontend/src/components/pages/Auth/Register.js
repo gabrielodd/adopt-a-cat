67c7f958a6d843e8ab9046109790de2e13d1176a
gabrielodd <gabrieloddone@id.uff.br>
2022-12-07 21:54:08 -0300

    Página e form de registro do usuário

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,19 @@
+import Input from '../../form/Input'
+
 function Register() {
+  function handleChange(e) {
+
+  }
+
   return (
     <section>
       <h1>Register</h1>
+      <form>
+        <Input text="Nome" type="text" name="name" placeholder="Digite" handleOnChange={handleChange}/>
+        <Input text="Nome" type="text" name="name" placeholder="Digite" handleOnChange={handleChange}/>
+        <Input text="Nome" type="text" name="name" placeholder="Digite" handleOnChange={handleChange}/>
+        <Input text="Nome" type="text" name="name" placeholder="Digite" handleOnChange={handleChange}/>
+      </form>
     </section>
   )
 }
