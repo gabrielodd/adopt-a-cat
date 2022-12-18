3db484622d29e953a61d4abec7b7c954718e95be
gabrielodd <gabrieloddone@id.uff.br>
2022-12-17 22:29:00 -0300

    Form and Login Function

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -5,11 +5,11 @@ import useAuth from "../hooks/useAuth";
 const Context = createContext();
 
 function UserProvider({ children }) {
-  const { authenticated, register, logout } = useAuth();
+  const { authenticated, register, logout, login } = useAuth();
 
   return (
     <Context.Provider
-      value={{ authenticated, register, logout }}
+      value={{ authenticated, register, logout, login }}
     >
       {children}
     </Context.Provider>
